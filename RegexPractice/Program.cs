1b9702f9db098fd72965455131f78e7360bf92f5
Reholly <eu.ustiantsev@yandex.ru>
2023-12-10 10:34:38 +0500

    added IsMatch example

---
+++ RegexPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+/* Пояснение регулярного выражения ^\d*\D+\d+$
+ * В начале >=0 символов цифр (\d*),
+ * потом >= 1 не цифр (\D+),
+ * потом >=1 цифровых символов (\d+),
+ * затем строка закончится ($).
+ */
+
+Regex matchRegex = new Regex(@"^\d*\D+\d+$");
+
+//Методы регулярных выражений. IsMatch()
+
+Console.WriteLine(matchRegex.IsMatch("1235678901")); //Вернёт false
+Console.WriteLine(matchRegex.IsMatch("abcdf12345")); // Вернет true
+
